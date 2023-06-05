321eb563b7bdd5a1b4799cc9a11ce6d1deb9d336
Keitiankor <keitian.kor@gmail.com>
2023-06-05 10:58:40 +0900

    [Add] p1654

---
+++ Class2/1654.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int k, n;
+	cin >> k >> n;
+	vector<int> lan;
+	for(int i = 0; i < k; i++) {
+		int temp;
+		cin >> temp;
+		lan.push_back(temp);
+	}
+	long min = 0;
+	long max = 2147483647;
+	long ans = 0;
+	while(min <= max) {
+		long mid   = (min + max) / 2;
+		long count = 0;
+		for(int i = 0; i < k; i++) { count += lan[i] / mid; }
+		if(count < n)
+			max = mid - 1;
+		else {
+			if(ans < mid) ans = mid;
+			min = mid + 1;
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
