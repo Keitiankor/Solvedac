5f1e6e76f07097b6623a4ee327af365343869dcf
Keitiankor <keitian.kor@gmail.com>
2023-06-05 13:41:11 +0900

    [Add] p18111

---
+++ Class2/18111.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<int> landscape;
+
+	int n, m, b;
+	cin >> n >> m >> b;
+
+	for(int i = 0; i < n * m; i++) {
+		int temp;
+		cin >> temp;
+		landscape.push_back(temp);
+	}
+
+	sort(landscape.begin(), landscape.end());
+
+	int low = landscape[0];
+
+	int ans = 2100000000;
+	int height;
+
+	while(true) {
+		bool isValid = false;
+		int fill	 = 0;
+		int cut		 = 0;
+		for(int i = 0; i < n * m; i++) {
+			if(landscape[i] < low) fill += low - landscape[i];
+			if(landscape[i] > low) cut += landscape[i] - low;
+		}
+		if((b + cut) - fill >= 0) isValid = true;
+		if(isValid) {
+			int time = fill + cut * 2;
+			if(ans >= time) {
+				ans	   = time;
+				height = low;
+				low++;
+			} else {
+				break;
+			}
+		} else
+			break;
+	}
+
+	cout << ans << " " << height;
+
+	return 0;
+}
