54b29474a5410025033f13f454417af3b4728a32
Keitiankor <keitian.kor@gmail.com>
2023-06-05 15:11:12 +0900

    [Add] p11399

---
+++ Class3/11399.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> p(n);
+	for(int i = 0; i < n; i++) cin >> p[i];
+
+	sort(p.begin(), p.end());
+
+	int sum = 0;
+
+	for(int i = 0; i < n; i++) sum += p[i] * (n - i);
+
+	cout << sum;
+
+	return 0;
+}
