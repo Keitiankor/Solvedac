b48a1fc7d01c9ed0056aa71ff3955a8e10da7318
Keitiankor <keitian.kor@gmail.com>
2023-06-05 12:37:24 +0900

    [Add] p15829

---
+++ Class2/15829.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	const int r = 31;
+	const int m = 1234567891;
+
+	long long hash = 0;
+
+	int l;
+	cin >> l;
+	string s;
+	cin >> s;
+
+	for(int i = 0; i < l; i++) {
+		long temp = s[i] - 'a' + 1;
+		long ri	  = 1;
+		for(int j = 0; j < i; j++) ri = (ri * r) % m;
+		hash += (long long)temp * ri;
+		hash %= m;
+	}
+
+	cout << hash;
+
+	return 0;
+}
