934717c2cbd67dc4cd342e01976a7195d25acb79
Keitiankor <keitian.kor@gmail.com>
2023-06-06 21:24:19 +0900

    [Add] p25083, p27866

---
+++ Class1/25083.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s = "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |\n";
+
+	cout << s;
+
+	return 0;
+}
---
+++ Class1/27866.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s;
+	cin >> s;
+	int i;
+	cin >> i;
+	cout << s[i - 1];
+
+	return 0;
+}
