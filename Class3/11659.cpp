3cebab9429d37819170d5f9d6f3270558e1cbb60
Keitiankor <keitian.kor@gmail.com>
2023-06-07 02:01:50 +0900

    [Add] p11659

---
+++ Class3/11659.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    int* sum = new int[n + 1];
+    sum[0]   = 0;
+    for(int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        sum[i + 1] = sum[i] + t;
+    }
+    for(int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << sum[b] - sum[a - 1] << '\n';
+    }
+
+    return 0;
+}
