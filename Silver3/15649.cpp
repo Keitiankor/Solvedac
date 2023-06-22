fc5507b46a574afeed1e13d3b12e84c91ec8bea7
Keitiankor <keitian.kor@gmail.com>
2023-06-22 15:21:51 +0900

    [Add] p15649

---
+++ Silver3/15649.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#    define NDEBUG
+#endif
+
+bool visited[9] = {false};
+int out[9];
+
+void bt(int n, int m, int depth) {
+    if(depth == m) {
+        for(int i = 0; i < m; i++)
+            cout << out[i] << " ";
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            out[depth] = i;
+            bt(n, m, depth + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+#ifdef NDEBUG
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+
+    int n, m;
+    cin >> n >> m;
+
+    bt(n, m, 0);
+
+    return 0;
+}
