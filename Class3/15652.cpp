d587d2d503239aebbf47c1149b7af5efd6b021c0
Keitiankor <keitian.kor@gmail.com>
2023-06-22 15:47:40 +0900

    [Add] p15652

---
+++ Class3/15652.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#    define NDEBUG
+#endif
+
+int out[9];
+int sum[9];
+void bt(int n, int m, int depth) {
+    if(depth == m) {
+        for(int i = 1; i < m; i++)
+            if(out[i - 1] > out[i])
+                return;
+
+        for(int i = 0; i < m; i++)
+            cout << out[i] << " ";
+
+        cout << '\n';
+
+        return;
+    }
+    for(int i = 1; i <= n; i++) {
+        out[depth] = i;
+        bt(n, m, depth + 1);
+    }
+}
+
+int main() {
+#ifdef NDEBUG
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+
+    out[0] = 9;
+
+    int n, m;
+    cin >> n >> m;
+
+    bt(n, m, 0);
+
+    return 0;
+}
